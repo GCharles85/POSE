4a13f77c129f083fb5749cd6dc4f7b5366dfb19b
GCharles85 <gcharles345@gmail.com>
2024-05-21 12:47:20 -0400

    Added components for storing URLs in communities page

---
+++ src/lib/components/AddUrl.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { urlStore, Url } from '../stores/urlStore';
+    import { get } from 'svelte/store';
+  
+    let newUrl = '';
+  
+    const addUrl = () => {
+      if (newUrl.trim()) {
+        urlStore.update(urls => {
+          const id = urls.length ? urls[urls.length - 1].id + 1 : 1;
+          return [...urls, { id, url: newUrl }];
+        });
+        newUrl = '';
+      }
+    };
+  </script>
+  
+  <div>
+    <input type="text" bind:value={newUrl} placeholder="Enter URL" />
+    <button on:click={addUrl}>Add URL</button>
+  </div>
+  
---
+++ src/lib/components/UrlList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { urlStore, Url } from '../stores/urlStore';
+    import { get } from 'svelte/store';
+  
+    let urls: Url[] = [];
+  
+    urlStore.subscribe(value => {
+      urls = value;
+    });
+  </script>
+  
+  <ul>
+    {#each urls as { id, url }}
+      <li key={id}>
+        <a href={url} target="_blank" rel="noopener noreferrer">{url}</a>
+      </li>
+    {/each}
+  </ul>
+  
